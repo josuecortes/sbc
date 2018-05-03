cab76e0a1e5bb2a303ce30125700358a1dd2da08
Josue Cortes <josue.cort3s@gmail.com>
2018-05-03 09:42:37 -0300

    Volta do pagseguro

--- app/controllers/transacao_controller.rb
+++ app/controllers/transacao_controller.rb
@@ -25,10 +25,8 @@ class TransacaoController < ApplicationController
 
     resposta = pagamento.register
 
-    puts 'ini res'
     puts resposta.errors.count
-    puts 'end res'
-
+    
     if resposta
       #return true
 
@@ -49,105 +47,177 @@ class TransacaoController < ApplicationController
   end
 
   def plano
-  	
+
+  	@configuracao = Configuracao.first
+
+    pagamento = PagSeguro::PaymentRequest.new
+
+    @licencas = params["licencas"].to_i
+    @estados = params["estados"]
+    @valor = params["valor"]
+    @dias = params["dias"].to_i
+    @codigo = params["codigo"].to_s
+
+    puts params
+
+    if @licencas > 0 
+      valor = @licencas * ((@dias * @configuracao.valor_licenca) / 365)
+      adicionais = @licencas
+      item = PagSeguro::Item.new(amount: valor, description: 'Licenças adicionais', quantity: adicionais, id: 1)
+      pagamento.items << item
+    end
+
+    if @estados
+      @estados.each do |e|
+        puts e
+        puts '-----'
+        descricao = "Estado - #{e}"
+        valor = 1 * ((@dias * @configuracao.valor_estado) / 365)
+        item = PagSeguro::Item.new(amount: valor, description: descricao, id: e)
+        pagamento.items << item 
+      end
+    end
+    
+    i = PagSeguro::Item.new(amount: 0.01, description: 'Plano Adicional', quantity: 1, id: @codigo)
+    pagamento.items << i
+
+    resposta = pagamento.register
+
+    if resposta
+      #return true
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: {link: resposta.url, retorno: true} }
+      end
+
+    else
+      #return false
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: {link: "", retorno: false} }
+      end
+
+    end
+
   end
 
   def notificacao_pagseguro
     headers['Access-Control-Allow-Origin'] = '*'
     transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
     if transaction and transaction.errors.empty? and transaction.status and transaction.status.id == "3"
-      user = User.where(email: transaction.sender.email).first
-      if user
-        #USUARIO EXISTE
-        empresa = user.empresa
-        plano = empresa.planos.new
-        plano.preco = transaction.gross_amount.to_s
-        plano.desconto = transaction.discount_amount.to_s
+
+
+      if transaction.items.store.last.description == 'Plano Adicional'
+        plano = Plano.where(transaction_code: transaction.items.store.last.id).first
 
         transaction.items.store.select{|c|c.description.include?("Estado")}.each do |e|
           plano.estados << e.description.gsub("Estado - ","")
         end
 
-        plano.inicio = transaction.updated_at
-        plano.fim = transaction.updated_at + 1.year
-
         if transaction.items.store[0].description == "Licenças adicionais"
-          plano.licencas = transaction.items.store[0].quantity
-        else
-          plano.licencas = 2
+          atual = plano.licencas + transaction.items.store[0].quantity
+          plano.licencas = atual
         end
-        
-        plano.empresa_id = empresa.id
-        plano.user_ids << user.id
 
-        if plano.save
-          puts "plano salvo"
-        else
-          puts "erro a salavar o plano"
-        end
+        plano.save
 
       else
-        #CRIAR USUARIO
-        u = User.new
-        u.nome = transaction.sender.name
-        u.email = transaction.sender.email
-        u.password = password_confirmation = "12345678"
-        u.permissao = "EMPRESA"
-        u.ativo = true
-        u.mudar_senha = false
+        user = User.where(email: transaction.sender.email).first
+        if user
+          #USUARIO EXISTE
+          empresa = user.empresa
+          plano = empresa.planos.new
+          plano.preco = transaction.gross_amount.to_s
+          plano.desconto = transaction.discount_amount.to_s
 
-        if u.save
-          puts "Usuario #{u.email} criado!!!"
-          puts "Criando Empresa..."
-          e = Empresa.new
-          e.nome = "Empresa de #{u.nome}"
-          if e.save
-            puts "Empresa #{e.nome} criada!!!"
-            puts "Adicionando usuário #{e.nome} a empresa #{e.nome}..."
-            u.empresa_id = e.id
-            if u.save
-              puts "Usuário #{e.nome} adicionado a empresa #{e.nome}!!!"
-            else
-              puts "Erro ao adicionar usuário a empresa"
-            end
+          transaction.items.store.select{|c|c.description.include?("Estado")}.each do |e|
+            plano.estados << e.description.gsub("Estado - ","")
+          end
 
-            plano = e.planos.new
+          plano.inicio = transaction.updated_at
+          plano.fim = transaction.updated_at + 1.year
 
-            plano.preco = transaction.gross_amount.to_s
-            plano.desconto = transaction.discount_amount.to_s
-
-            transaction.items.store[1..-1].each do |e|
-              plano.estados << e.description.gsub("Estado - ","")
-            end
-
-            plano.inicio = transaction.updated_at
-            plano.fim = transaction.updated_at + 1.year
-            
-            if transaction.items.store[0].description == "Licenças adicionais"
-              plano.licencas = transaction.items.store[0].quantity
-            else
-              plano.licencas = 2
-            end
-            
-            plano.empresa_id = e.id
-            plano.user_ids << u.id
-            plano.transaction_code = transaction.code
-
-            if plano.save
-              puts "plano salvo"
-            else
-              puts "erro a salavar o plano"
-            end
-
+          if transaction.items.store[0].description == "Licenças adicionais"
+            plano.licencas = transaction.items.store[0].quantity
           else
-            puts "Erro ao criar empresa #{e.nome}!!!"
+            plano.licencas = 2
+          end
+          
+          plano.empresa_id = empresa.id
+          plano.user_ids << user.id
+
+          if plano.save
+            puts "plano salvo"
+          else
+            puts "erro a salavar o plano"
           end
 
         else
-          puts "Erro ao criar o usuario #{u.email}!!!"
-        end
+          #CRIAR USUARIO
+          u = User.new
+          u.nome = transaction.sender.name
+          u.email = transaction.sender.email
+          u.password = password_confirmation = "12345678"
+          u.permissao = "EMPRESA"
+          u.ativo = true
+          u.mudar_senha = false
 
+          if u.save
+            puts "Usuario #{u.email} criado!!!"
+            puts "Criando Empresa..."
+            e = Empresa.new
+            e.nome = "Empresa de #{u.nome}"
+            if e.save
+              puts "Empresa #{e.nome} criada!!!"
+              puts "Adicionando usuário #{e.nome} a empresa #{e.nome}..."
+              u.empresa_id = e.id
+              if u.save
+                puts "Usuário #{e.nome} adicionado a empresa #{e.nome}!!!"
+              else
+                puts "Erro ao adicionar usuário a empresa"
+              end
+
+              plano = e.planos.new
+
+              plano.preco = transaction.gross_amount.to_s
+              plano.desconto = transaction.discount_amount.to_s
+
+              transaction.items.store[1..-1].each do |e|
+                plano.estados << e.description.gsub("Estado - ","")
+              end
+
+              plano.inicio = transaction.updated_at
+              plano.fim = transaction.updated_at + 1.year
+              
+              if transaction.items.store[0].description == "Licenças adicionais"
+                plano.licencas = transaction.items.store[0].quantity
+              else
+                plano.licencas = 2
+              end
+              
+              plano.empresa_id = e.id
+              plano.user_ids << u.id
+              plano.transaction_code = transaction.code
+
+              if plano.save
+                puts "plano salvo"
+              else
+                puts "erro a salavar o plano"
+              end
+
+            else
+              puts "Erro ao criar empresa #{e.nome}!!!"
+            end
+
+          else
+            puts "Erro ao criar o usuario #{u.email}!!!"
+          end
+
+        end
       end
+        
     end
   end
 end
